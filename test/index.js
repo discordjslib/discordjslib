26d53882ec080ea94eaf85bd4db9bf1b1c49cf69
SamHodge07 <sammyhodge8@gmail.com>
2021-04-17 12:44:45 +0100

    Updates

--- test/index.js
+++ test/index.js
@@ -1,3 +1,3 @@
-const Discord = require('../Lib/Classes/Client/Client')
-const client = new Discord(token)
-client.channels.find()
+const Discord = require('../lib/Discord')
+const client = new Discord().Client('ODE4MDkyMDQyOTgxODY3NTcw.YETBlg.KUFanXM6ZBSsp9Ek68bpUDBZQ1U')
+console.log(client.user)
