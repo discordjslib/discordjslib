57bc018a303ca9e971b8575b5b01f1e8f22a3c8b
SamHodge07 <sammyhodge8@gmail.com>
2021-04-18 21:54:35 +0100

    updates

--- test/index.js
+++ test/index.js
@@ -1,8 +1,8 @@
 const Discord = require('../lib/Discord')
-const client = new Discord().Client('')
+const client = new Discord().Client('ODE4MDkyMDQyOTgxODY3NTcw.YETBlg.w07RZvhk1S8zOV-9LdFV6XWL97k')
 const run = async() =>{
 const channel = await client.channels.find('825019102266916864')
-const guild = await channel.guild
-console.log(await guild.owner)
+console.log(channel.name)
+console.log(await channel.messages.find('833438582672064533'))
 }
 run()
