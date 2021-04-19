be5624b2103efed727998cab8d9905aa672ba939
SamHodge07 <sammyhodge8@gmail.com>
2021-04-19 18:16:35 +0100

    updates

--- test/index.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('../lib/Discord')
-const client = new Discord().Client('')
-const run = async() =>{
-const channel = await client.channels.find('825019102266916864')
-console.log(channel.name)
-console.log(await channel.messages.find('833438582672064533'))
-}
-run()
