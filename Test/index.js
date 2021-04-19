18b04fbe19a84dbf66711e314213be2dcc3ca592
discordjslib <71181869+discordjslib@users.noreply.github.com>
2021-04-19 18:20:24 +0100

    Update index.js

--- Test/index.js
+++ Test/index.js
@@ -1,5 +1,5 @@
 const Discord = require('../index')
-const client = new Discord().Client('ODE4MDkyMDQyOTgxODY3NTcw.YETBlg.JP-TI4lWXHpkQOkEhXGgwqp0z4g')
+const client = new Discord().Client('')
 const run = async() =>{
 const channel = await client.channels.find('825019102266916864')
     const message = await channel.messages.find('833438582672064533')
