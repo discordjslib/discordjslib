51d4ab55ae592eaad4a547e8475d3c2246a0a6b8
SamHodge07 <sammyhodge8@gmail.com>
2021-04-19 18:48:43 +0100

    formatting

--- Test/index.js
+++ Test/index.js
@@ -1,8 +1,8 @@
 const Discord = require('../index')
 const client = new Discord().Client('')
-const run = async() =>{
-const channel = await client.channels.find('825019102266916864')
+const run = async () => {
+    const channel = await client.channels.find('825019102266916864')
     const message = await channel.messages.find('833438582672064533')
-console.log(channel)
+    console.log(channel)
 }
 run()
